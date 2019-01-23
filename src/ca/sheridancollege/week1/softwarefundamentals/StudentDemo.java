2e9293c57236dd66d5d1a8eafb46399ce69eb58a
Neil Froggatt <neilfr@live.com>
2019-01-23 14:30:22 -0500

    added a new date to a comment

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
@@ -1,4 +1,5 @@
 /*
+* January 23rd... edit
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
